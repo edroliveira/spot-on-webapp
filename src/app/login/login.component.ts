8dcb68cb2047535e02a40b0b8aa0aa8f024ed8bf
Ednaldo Romer de Oliveira Júnior <ednaldojunior2001@gmail.com>
2024-06-04 23:32:52 -0300

    Updating Angular and Angular Material to  version to 18

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,11 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -26,8 +31,19 @@ export class LoginComponent {
     senha: new FormControl({ value: '', disabled: false }, Validators.required)
   });
 
+  constructor(private _snackBar: MatSnackBar) { }
+
   onClickLogin() {
     console.log(this.loginForm);
+    this.openAlert();
+  }
+
+  openAlert() {
+    this._snackBar.open('Preencha os campos obrigatórios', 'OK', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 3000
+    });
   }
 
 }
