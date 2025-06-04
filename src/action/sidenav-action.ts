c5ea9a7b13761f3dd55d8f1c9bcdd43e66e5ccc1
Ednaldo Romer de Oliveira Júnior <ednaldojunior2001@gmail.com>
2025-06-04 10:55:08 -0300

    Adding a sidenav and global triggering for it's toggle

---
+++ src/action/sidenav-action.ts
@@ -0,0 +1,5 @@
+export class SetToggleSidenav {
+    static readonly type = '[Sidenav] SetToggleSidenav';
+
+    constructor(public toggleSidenav: boolean) { }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthGoogleService } from '../../services/auth-google.service';
 import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
+import { distinctUntilChanged, Observable } from 'rxjs';
 import { GoogleUserData } from '../../models/google-user-data';
 import { GoogleUserState } from '../../state/google-user-state';
 import { select, Store } from '@ngxs/store';
 import { HeaderComponent } from '../header/header.component';
+import { SharedModule } from '../shared/shared.module';
+import { SidenavState } from '../../state/sidenav-state';
+import { SetToggleSidenav } from '../../action/sidenav-action';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,21 +17,37 @@ import { HeaderComponent } from '../header/header.component';
   providers: [AuthGoogleService],
   imports: [
     AsyncPipe,
-    HeaderComponent
+    HeaderComponent,
+    SharedModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
-  currentUser$: Observable<GoogleUserData | null> = this.store.select(GoogleUserState.getCurrentUser);
+  currentUser$: Observable<GoogleUserData | null> = this.store.select(GoogleUserState.getCurrentUser)
+    .pipe(distinctUntilChanged());
+  toggleSidenav$: Observable<boolean> = this.store.select(SidenavState.getToggleSidenav)
+    .pipe(distinctUntilChanged());
+
+  @ViewChild('drawer') drawer!: MatDrawer;
 
   constructor(
     private store: Store,
     private authGoogleService: AuthGoogleService
-  ) { }
+  ) {
+    this.handleToggleSidenav();
+  }
 
   ngOnInit(): void {
+    this.store.dispatch(new SetToggleSidenav(false));
+  }
 
+  handleToggleSidenav(): void {
+    this.toggleSidenav$.subscribe((toggleSidenav) => {
+      if (this.drawer) {
+        toggleSidenav ? this.drawer.open() : this.drawer.close();
+      }
+    });
   }
 
 }
